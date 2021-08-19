c1f884d8ab298eeae76eca7ef65bf617c8aaca91
halki <s.halki0203@icloud.com>
2021-08-19 20:16:05 +0900

    アバター画像編集
    アバター画像がプレビューで表示される様にした

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,3 +21,15 @@
     </div>
 </div>
 @endsection
+
+<script>
+    document.querySelector('.image-picker input')
+    .addEventListener('change', (e) => {
+        const input = e.target;
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            input.closest('.image-picker').querySelector('img').src = e.target.result
+        };
+        reader.readAsDataURL(input.files[0]);
+    });
+</script>
