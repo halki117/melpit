4de9a00d07a411c53f896c4ab854b182426e8845
halki <s.halki0203@icloud.com>
2021-08-29 22:09:15 +0900

    Update SellController.php

--- app/Http/Controllers/SellController.php
+++ app/Http/Controllers/SellController.php
@@ -5,15 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\ItemCondition;
+use App\PrimaryCategory;
 
 class SellController extends Controller
 {
     public function showSellForm()
 
     {
-
+        $categories = PrimaryCategory::orderBy('sort_no')->get();
         $conditions = ItemCondition::orderBy('sort_no')->get();
 
-        return view('sell')->with('conditions', $conditions);
+        return view('sell')
+        ->with('categories', $categories)
+        ->with('conditions', $conditions);
     }
 }
